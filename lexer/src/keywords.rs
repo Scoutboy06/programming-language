8f5c0e004d6157acadb1060afd2443e883006f97
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-01-17 19:48:07 +0100

    Switch from lazy_static to phf crate

--- lexer/src/keywords.rs
+++ lexer/src/keywords.rs
@@ -1,5 +1,4 @@
-use lazy_static::lazy_static;
-use std::collections::HashMap;
+use phf::phf_map;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Keyword {
@@ -45,70 +44,59 @@ pub enum Keyword {
     StringType,  // : string
     NumberType,  // : number
     BooleanType, // : boolean
-    RecordType,  // : Record<>
-    ArrayType,   // : Array<>
+    Enum,        // enum Foo {}
 }
 
-lazy_static! {
-    static ref KEYWORDS: HashMap<&'static str, Keyword> = {
-        let mut m = HashMap::new();
+static KEYWORDS: phf::Map<&'static str, Keyword> = phf_map! {
+    "var" => Keyword::Var,
+    "let" => Keyword::Let,
+    "const" => Keyword::Const,
+    "function" => Keyword::Function,
+    "return" => Keyword::Return,
+    "async" => Keyword::Async,
+    "await" => Keyword::Await,
+    "static" => Keyword::Static,
+    "if" => Keyword::If,
+    "else" => Keyword::Else,
+    "try" => Keyword::Try,
+    "catch" => Keyword::Catch,
+    "finally" => Keyword::Finally,
+    "while" => Keyword::While,
+    "do" => Keyword::Do,
+    "for" => Keyword::For,
+    "in" => Keyword::In,
+    "of" => Keyword::Of,
+    "break" => Keyword::Break,
+    "continue" => Keyword::Continue,
+    "class" => Keyword::Class,
+    "abstract" => Keyword::Abstract,
+    "extends" => Keyword::Extends,
+    "implements" => Keyword::Implements,
+    "new" => Keyword::New,
+    "this" => Keyword::This,
+    "super" => Keyword::Super,
+    "private" => Keyword::Private,
+    "protected" => Keyword::Protected,
+    "switch" => Keyword::Switch,
+    "case" => Keyword::Case,
+    "default" => Keyword::Default,
+    "type" => Keyword::Type,
+    "interface" => Keyword::Interface,
+    "true" => Keyword::True,
+    "false" => Keyword::False,
 
-        // JavaScript keywords
-        m.insert("var", Keyword::Var);
-        m.insert("let", Keyword::Let);
-        m.insert("const", Keyword::Const);
-        m.insert("function", Keyword::Function);
-        m.insert("return", Keyword::Return);
-        m.insert("async", Keyword::Async);
-        m.insert("await", Keyword::Await);
-        m.insert("static", Keyword::Static);
-        m.insert("if", Keyword::If);
-        m.insert("else", Keyword::Else);
-        m.insert("try", Keyword::Try);
-        m.insert("catch", Keyword::Catch);
-        m.insert("finally", Keyword::Finally);
-        m.insert("while", Keyword::While);
-        m.insert("do", Keyword::Do);
-        m.insert("for", Keyword::For);
-        m.insert("in", Keyword::In);
-        m.insert("of", Keyword::Of);
-        m.insert("break", Keyword::Break);
-        m.insert("continue", Keyword::Continue);
-        m.insert("class", Keyword::Class);
-        m.insert("abstract", Keyword::Abstract);
-        m.insert("extends", Keyword::Extends);
-        m.insert("implements", Keyword::Implements);
-        m.insert("new", Keyword::New);
-        m.insert("this", Keyword::This);
-        m.insert("super", Keyword::Super);
-        m.insert("private", Keyword::Private);
-        m.insert("protected", Keyword::Protected);
-        m.insert("switch", Keyword::Switch);
-        m.insert("case", Keyword::Case);
-        m.insert("default", Keyword::Default);
-        m.insert("type", Keyword::Type);
-        m.insert("interface", Keyword::Interface);
-        m.insert("true", Keyword::True);
-        m.insert("false", Keyword::False);
-
-        // TypeScript types
-        m.insert("string", Keyword::StringType);
-        m.insert("number", Keyword::NumberType);
-        m.insert("boolean", Keyword::BooleanType);
-        m.insert("Record", Keyword::RecordType);
-        m.insert("Array", Keyword::ArrayType);
-
-        m
-    };
-}
+    // TypeScript keywords
+    "string" => Keyword::StringType,
+    "number" => Keyword::NumberType,
+    "boolean" => Keyword::BooleanType,
+    "enum" => Keyword::Enum,
+};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum TypeKeyword {
     String,
     Number,
     Boolean,
-    Record,
-    Array,
 }
 
 impl Keyword {
@@ -121,8 +109,6 @@ impl Keyword {
             Self::StringType => Some(TypeKeyword::String),
             Self::NumberType => Some(TypeKeyword::Number),
             Self::BooleanType => Some(TypeKeyword::Boolean),
-            Self::RecordType => Some(TypeKeyword::Record),
-            Self::ArrayType => Some(TypeKeyword::Array),
             _ => None,
         }
     }
