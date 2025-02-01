ca09e8c0779a6e92667063ec73fae885a28e5ec2
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-02-01 19:57:42 +0100

    Updated match clause

--- semantic/src/types.rs
+++ semantic/src/types.rs
@@ -24,15 +24,15 @@ impl ExpressionType {
     pub fn matches(&self, ann_type: &TypeValue) -> bool {
         match ann_type {
             TypeValue::TypeLiteral(type_literal) => match type_literal.literal {
-                Literal::BooleanLiteral(_) => matches!(self, Self::Boolean),
-                Literal::NumberLiteral(_) => matches!(self, Self::Number),
-                Literal::NullLiteral(_) => matches!(self, Self::Null),
-                Literal::StringLiteral(_) => matches!(self, Self::String),
+                Literal::BooleanLiteral(_) => *self == Self::Boolean,
+                Literal::NumberLiteral(_) => *self == Self::Number,
+                Literal::NullLiteral(_) => *self == Self::Null,
+                Literal::StringLiteral(_) => *self == Self::String,
             },
             TypeValue::KeywordType(keyword_type) => match keyword_type.kind {
-                TypeKeyword::String => matches!(self, Self::String),
-                TypeKeyword::Number => matches!(self, Self::Number),
-                TypeKeyword::Boolean => matches!(self, Self::Boolean),
+                TypeKeyword::String => *self == Self::String,
+                TypeKeyword::Number => *self == Self::Number,
+                TypeKeyword::Boolean => *self == Self::Boolean,
             },
             TypeValue::TypeReference(_type_reference) => todo!(),
             TypeValue::ArrayType(_array_type) => todo!(),
