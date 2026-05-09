ca324bd3baa7adb2c0bc8718337077497910aa9f
Elias Wennerlund <elias06wennerlund@gmail.com>
2026-05-09 13:26:03 +0200

    Refactor lexer integration tests to replace operator tokens with specific token types

--- lexer/tests/integration_test.rs
+++ lexer/tests/integration_test.rs
@@ -1,4 +1,4 @@
-use lexer::{AssignmentOperator, BinaryOperator, Keyword, Lexer, TokenKind, TokenValue};
+use lexer::{Keyword, Lexer, TokenKind, TokenValue};
 use pretty_assertions::assert_eq;
 
 fn expect_tokens(source_code: &str, expected_tokens: &[(TokenKind, TokenValue)]) {
@@ -23,11 +23,11 @@ fn numbers() {
         &source_code,
         &vec![
             (TK::Number, TV::Number(8.0)),
-            (TK::Operator, TV::Operator(BinaryOperator::Plus.into())),
+            (TK::Plus, TV::None),
             (TK::Number, TV::Number(5.0)),
-            (TK::Operator, TV::Operator(BinaryOperator::Minus.into())),
+            (TK::Minus, TV::None),
             (TK::Number, TV::Number(2.0)),
-            (TK::Operator, TV::Operator(BinaryOperator::Div.into())),
+            (TK::Slash, TV::None),
             (TK::Number, TV::Number(2.0)),
         ],
     );
@@ -43,12 +43,9 @@ fn let_statement() {
         &vec![
             (TK::Keyword, TV::Keyword(Keyword::Let)),
             (TK::Identifier, TV::Identifier("x".into())),
-            (
-                TK::Operator,
-                TV::Operator(AssignmentOperator::Assign.into()),
-            ),
+            (TK::Eq, TV::None),
             (TK::Number, TV::Number(123.0)),
-            (TK::Operator, TV::Operator(BinaryOperator::Plus.into())),
+            (TK::Plus, TV::None),
             (TK::Number, TV::Number(456.0)),
             (TK::Semi, TV::None),
         ],
@@ -92,11 +89,8 @@ fn string_literal() {
         &vec![
             (TK::Keyword, TV::Keyword(Keyword::Let)),
             (TK::Identifier, TV::Identifier("x".into())),
-            (
-                TK::Operator,
-                TV::Operator(AssignmentOperator::Assign.into()),
-            ),
-            (TK::String, TV::String("'This is a string literal'".into())),
+            (TK::Eq, TV::None),
+            (TK::String, TV::String("This is a string literal".into())),
             (TK::Semi, TV::None),
         ],
     );
@@ -113,10 +107,7 @@ fn template_string_literal() {
         &vec![
             (TK::Keyword, TV::Keyword(Keyword::Let)),
             (TK::Identifier, TV::Identifier("x".into())),
-            (
-                TK::Operator,
-                TV::Operator(AssignmentOperator::Assign.into()),
-            ),
+            (TK::Eq, TV::None),
             (
                 TK::String,
                 TV::String(
@@ -172,10 +163,7 @@ fn if_statement_with_variables() {
             (TK::Identifier, TV::Identifier("pos".into())),
             (TK::Dot, TV::None),
             (TK::Identifier, TV::Identifier("x".into())),
-            (
-                TK::Operator,
-                TV::Operator(BinaryOperator::GreaterThan.into()),
-            ),
+            (TK::Gt, TV::None),
             (TK::Identifier, TV::Identifier("window".into())),
             (TK::Dot, TV::None),
             (TK::Identifier, TV::Identifier("innerWidth".into())),
